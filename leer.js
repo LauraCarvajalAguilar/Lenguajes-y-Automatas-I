fda648ae27b77a0b32c5588f00199bf48d7787f7
Jazmin <43412480+JazminNahuatPacheco@users.noreply.github.com>
2019-10-11 20:06:23 -0500

    Update leer.js

--- leer.js
+++ leer.js
@@ -23,3 +23,31 @@ guardar.onload = function comentarios(){
     document.getElementById("comentarios").textContent = this.result;
   }
   
+document.getElementById("buscando").addEventListener("click",Buscar);
+  var codigo="public class pila { int n; /* TAMAÑO DE LA PILA O NUMERO DE ELEMETOS */ int tope; boolean estado; /* TRUE = PILA ESTA VACIA */ int [] datos;  public pila(int tam) /* PEDIMOS EL TAMAÑO DE LA PILA  PARA CREARLA */ {this.n=tam; datos = new int [tam]; tope=-1; /* APUNTA A LA PRIMERA POSICION */ } public void apilar (int valor) /* SE PIDE AL VALOR PARA APILAR */ { tope++; datos[tope]= valor; }  public int Desapilar(){int valor = datos[tope]; datos[tope]=0; tope--; return valor; } ";
+  function Buscar(){
+    let mensaje2=codigo.match(/[a-z]+[<|>|=]+[1-9]/g);
+    document.getElementById("Buscar").innerHTML=newmensaje2;
+  }
+  
+  document.getElementById("Encriptado").addEventListener("click", Encriptar);
+  var codigo="public class pila { int n; /* TAMAÑO DE LA PILA O NUMERO DE ELEMETOS */ int tope; boolean estado; /* TRUE = PILA ESTA VACIA */ int [] datos;  public pila(int tam) /* PEDIMOS EL TAMAÑO DE LA PILA  PARA CREARLA */ {this.n=tam; datos = new int [tam]; tope=-1; /* APUNTA A LA PRIMERA POSICION */ } public void apilar (int valor) /* SE PIDE AL VALOR PARA APILAR */ { tope++; datos[tope]= valor; }  public int Desapilar(){int valor = datos[tope]; datos[tope]=0; tope--; return valor; } ";
+  
+  function Encriptar(){
+    var expresion = /["]{1}([\s]*[\w]*)+["]/gm;
+    var le = document.getElementById("parrafo").innerHTML;
+
+    var respuesta = le.match(expresion);
+    document.getElementById("comillas").innerHTML = respuesta;
+
+    var obj = document.getElementById("comillas").innerHTML;
+    var remplazar = obj.replace(/[e|E]/g,"p");
+    remplazar = remplazar.replace(/[p|P]/g,"b");
+    remplazar = remplazar.replace(/[o|O]/g,"s");
+    remplazar = remplazar.replace(/[n|N]/g,"i");
+    remplazar = remplazar.replace(/[g|G]/g,"a");
+    remplazar = remplazar.replace(/[r|R]/g,"i");
+    remplazar = remplazar.replace(/[m|M]/g,"q");
+
+    document.getElementById("Encriptar").innerHTML = remplazar;
+  }
