7df03895c7c1a755494d58d49d3feafc6823eaf8
Laura Carvajal <43411408+LauraCarvajalAguilar@users.noreply.github.com>
2019-10-11 20:09:42 -0500

    Update ejercicio5.java

--- ejercicio5.java
+++ ejercicio5.java
@@ -3,11 +3,20 @@ public class ejercicio5 {
 
     public static void main (String[] args){
     	
-    	Scanner sc= new Scanner(System.in);
-    	int n,res;
-    	System.out.println("Captura el valor...:");
+    	Scanner sc = new Scanner(System.in);
+    	int n, res;
+    	System.out.println("Captura el valor:");
     	n=sc.nextInt();
     	
       	System.out.println("Resultado.." + sumar_dig(n));
-    }
+}
    
+public static int sumar_dig(int n) { //INICIO METODO
+    if (n == 0) {  //INICIO IF    //caso base
+            return n;
+                } //FIN IF
+   else { //INICIO ELSE
+            return sumar_dig (n / 10) + (n % 10);
+        } //FIN ELSE 
+    } //FIN METODO
+}
