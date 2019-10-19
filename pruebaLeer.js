bfb383264196a3f8f4d53e40ca57c098a7079c1d
Laura Carvajal <43411408+LauraCarvajalAguilar@users.noreply.github.com>
2019-10-18 20:24:57 -0500

    Create pruebaLeer.js

---
+++ pruebaLeer.js
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <LINK REL=StyleSheet HREF="diseño.css" TYPE="text/css">
+    <title>Proyecto</title>
+</head>
+
+<body>
+   <center>
+   <h3>Tema 2. Proyecto de Lenguajes y Automatas I</h3>
+   </center>
+   <br>
+        <center><label><strong>Paso 1: </strong>Selecciona el archivo</label></center>
+        <br>
+        <center>
+        <input id="cuadro1" type="file" name="adjunto" accept=".java"/>
+        <br>
+        <textarea cols="100" rows="15" id="entrada"></textarea>
+        <br> <br> <br>
